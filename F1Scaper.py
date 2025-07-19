d37c896dc2473ee3a739367d11b259627743b80c
devlin-hahn04 <devlin.hahn@upr.edu>
2025-07-18 22:11:17 -0400

    supabase

--- F1Scaper.py
+++ F1Scaper.py
@@ -3,18 +3,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
 
 
 
 def getWDC():
 
-    chrome_options = Options()
-    chrome_options.binary_location = "/usr/bin/chromium-browser"  
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.binary_location = "/usr/bin/chromium-browser" 
 
-    driver = webdriver.Chrome(options=chrome_options)
+    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=options)
+
     driver.get("https://www.formula1.com/en/results/2025/drivers")
 
     # Wait until at least one driver row is visible
@@ -41,13 +45,15 @@ def getWDC():
 
 def getWCC():
 
-    chrome_options = Options()
-    chrome_options.binary_location = "/usr/bin/chromium-browser"  
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.binary_location = "/usr/bin/chromium-browser" 
 
-    driver= webdriver.Chrome(options=chrome_options)
+    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=options)
+    
     driver.get("https://www.formula1.com/en/results/2025/team")
 
 
