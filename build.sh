24c41c5231b30455a40e4d285fda6087f347db43
devlin-hahn04 <devlin.hahn@upr.edu>
2025-07-18 20:18:04 -0400

    yea

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get update
-apt-get install -y chromium-browser chromium-chromedriver
-
-echo "Chromium location: $(which chromium-browser || which chromium)"
-echo "Chromedriver location: $(which chromedriver)"
-
